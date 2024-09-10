08b51ebf2818a5801bfee9fa3287b4dbbd1e2554
grigoryan-m <waldm4rk@gmail.com>
2024-09-10 13:42:43 +0400

    Activity now displays in order and more organized

--- src/main/java/JsonParser.java
+++ src/main/java/JsonParser.java
@@ -4,28 +4,36 @@ import org.json.JSONArray;
 import java.util.HashMap;
 
 public class JsonParser {
-    public static void stringToArray(String str){
+    public static void stringToArray(String str) {
         JSONArray jsonArray = new JSONArray(str);
-        displayActivity(jsonArray);
+        countActivity(jsonArray);
     }
 
-    private static HashMap<String, Integer> countPushes(JSONArray jsonArray){
-        HashMap<String, Integer> pushes = new HashMap<>();
-        for(int i = 0; i < jsonArray.length(); i++){
-            JSONObject jsonObj = jsonArray.getJSONObject(i);
-            if(jsonObj.getString("type").equals("PushEvent")){
-                String repoName = jsonObj.getJSONObject("repo").getString("name");
-                if(pushes.containsKey(repoName)){
-                    pushes.put(repoName, pushes.get(repoName) + 1);
-                    continue;
-                }
-                pushes.put(repoName, 1);
+    private static void countActivity(JSONArray jsonArray) {
+        String lastActivity = jsonArray.getJSONObject(0).getString("type");
+        String repoName = jsonArray.getJSONObject(0).getJSONObject("repo").getString("name");
+        int numOfActivity = 1;
+
+        for (int i = 1; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            if (jsonObject.getString("type").equals(lastActivity) && jsonObject.getJSONObject("repo").getString("name").equals(repoName)) {
+                numOfActivity++;
+                continue;
             }
+            displayActivity(lastActivity, repoName, numOfActivity);
+            lastActivity = jsonObject.getString("type");
+            repoName = jsonObject.getJSONObject("repo").getString("name");
+            numOfActivity = 1;
         }
-        return pushes;
     }
 
-    private static void displayActivity(JSONArray jsonArray){
-        countPushes(jsonArray);
+    private static void displayActivity(String activity, String repoName, int numOfActivity) {
+        switch (activity) {
+            case "PushEvent" -> System.out.println("- Pushed " + numOfActivity + " commits to " + repoName);
+            case "IssuesEvent" -> System.out.println("- Created " + numOfActivity + " issues for " + repoName);
+            case "CreateEvent" -> System.out.println("- Created " + numOfActivity + " branches for " + repoName);
+        }
     }
+
+
 }
